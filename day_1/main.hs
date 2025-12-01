f17c5cbd7b8c0f6d66423156c4eb0b05b27cc18c
Alex Rosca <alex@rosca.xyz>
2025-12-01 20:43:36 +0100

    Reorder functions

--- day_1/main.hs
+++ day_1/main.hs
@@ -1,6 +1,19 @@
+main = do
+  input <- readFile "input.txt"
+
+  let list = rotations input
+
+  print $ part1 list
+  print $ part2 list
+
 rotations = map (convert . splitAt 1) . lines
   where
     convert (dir, dist) = (dir, read dist)
+
+part1 list = length pointing_at_0
+  where
+    turns = scanl turn 50 list
+    pointing_at_0 = filter (\x -> x `mod` 100 == 0) turns
 
 turn pos (dir, dist) = pos + dist * vec
   where
@@ -8,17 +21,4 @@ turn pos (dir, dist) = pos + dist * vec
       "L" -> -1
       "R" -> 1
 
-part1 list = length pointing_at_0
-  where
-    turns = scanl turn 50 list
-    pointing_at_0 = filter (\x -> x `mod` 100 == 0) turns
-
-part2 list =
-  part1 $ concatMap (\(dir, dist) -> replicate dist (dir, 1)) list
-
-main = do
-  input <- readFile "input.txt"
-  let list = rotations input
-
-  print $ part1 list
-  print $ part2 list
+part2 = part1 . concatMap (\(dir, dist) -> replicate dist (dir, 1))
