9ddec1ea65259bbdfa06907a5f18d94f204a42d6
Alex Rosca <alex@rosca.xyz>
2025-12-02 21:31:44 +0100

    Day 2, part 2

--- day_2/main.hs
+++ day_2/main.hs
@@ -6,6 +6,7 @@ main = do
   let ids = expandRanges input
 
   print $ part1 ids
+  print $ part2 ids
 
 expandRanges :: String -> [String]
 expandRanges = concatMap (expandRange . splitOn "-") . splitOn ","
@@ -17,3 +18,12 @@ invalidSum predicate = sum . map read . filter predicate
 part1 = invalidSum repeatedTwice
   where
     repeatedTwice id = uncurry (==) $ splitAt (length id `div` 2) id
+
+part2 = invalidSum repeated
+  where
+    repeated id = any repeating (chunks id)
+    chunks id = map ($ id) (chunkFunctions id)
+    chunkFunctions id = map chunksOf [1 .. length id `div` 2]
+
+repeating :: [String] -> Bool
+repeating (x : xs) = all (== x) xs
