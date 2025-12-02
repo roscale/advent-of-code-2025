b655bb9266ce130570b32389cbbe171d6530005d
Alex Rosca <alex@rosca.xyz>
2025-12-02 20:38:20 +0100

    Refactor in preparation for part 2

--- day_2/main.hs
+++ day_2/main.hs
@@ -1,18 +1,19 @@
 import Data.List.Extra
 
-expandRanges :: String -> [String]
-expandRanges = concatMap (expandRange . splitOn "-") . splitOn ","
-  where
-    expandRange [a, b] = map show [read a :: Int .. read b]
-
-isInvalid :: String -> Bool
-isInvalid id = uncurry (==) $ splitAt (length id `div` 2) id
-
-part1 = sum . map read . filter isInvalid
-
 main = do
   input <- readFile "input.txt"
 
   let ids = expandRanges input
 
   print $ part1 ids
+
+expandRanges :: String -> [String]
+expandRanges = concatMap (expandRange . splitOn "-") . splitOn ","
+  where
+    expandRange [a, b] = map show [read a :: Int .. read b]
+
+invalidSum predicate = sum . map read . filter predicate
+
+part1 = invalidSum repeatedTwice
+  where
+    repeatedTwice id = uncurry (==) $ splitAt (length id `div` 2) id
