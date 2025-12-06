6eb5dedadf33efb58e7e855d0341e57f618601c9
Alex Rosca <alex@rosca.xyz>
2025-12-06 18:20:16 +0100

    Day 6, part 2

--- day_6/main.hs
+++ day_6/main.hs
@@ -1,19 +1,30 @@
+import Data.Char
 import Data.List
+import Data.List.Extra
+import Data.List.Split
 
 main = do
   input <- readFile "input.txt"
-  let matrix = transpose $ map words $ lines input
-  let problems = map parseProblem matrix
+  let matrix = lines input
+  let problems = getProblems matrix
 
   print $ part1 problems
+  print $ part2 problems
+
+getProblems matrix = map transpose problems
+  where
+    problems = splitWhen (all isSpace) $ transpose matrix
 
 part1 problems = sum $ map evaluate problems
 
-parseProblem list = (op, terms)
+part2 problems = sum $ map (evaluate . transposeProblem) problems
+
+evaluate problem = foldl1 op terms
   where
-    op = case last list of
+    op = case last trimmedProblem of
       "*" -> (*)
       "+" -> (+)
-    terms = map read $ init list
+    terms = map read $ init trimmedProblem
+    trimmedProblem = map trim problem
 
-evaluate (op, terms) = foldl1 op terms
+transposeProblem problem = transpose (init problem) ++ [last problem]
