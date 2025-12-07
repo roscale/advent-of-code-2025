8f0caedc5d04aed94b5afdf829f6d812e13e5844
Alex Rosca <alex@rosca.xyz>
2025-12-07 16:14:30 +0100

    Day 7, part 2

--- day_7/main.hs
+++ day_7/main.hs
@@ -1,4 +1,5 @@
-import Data.Set hiding (take)
+import Data.Map
+import Data.Set
 
 main = do
   input <- readFile "input.txt"
@@ -7,25 +8,35 @@ main = do
   let array = concat matrix
 
   let (start : splitters) = Prelude.map fst $ Prelude.filter (\(_, e) -> e == 'S' || e == '^') $ zip (enumerate2D width) array
+  let splittersSet = Data.Set.fromList splitters
 
-  print $ part1 height start (fromList splitters)
+  print $ part1 height start splittersSet
+  print $ part2 height start splittersSet
 
 enumerate2D width = Prelude.map (coords width) [0 ..]
   where
     coords width i = (i `div` width, i `mod` width)
 
 part1 :: Int -> (Int, Int) -> Set (Int, Int) -> Int
-part1 height start splitters = fst $ last $ take height $ iterate (advanceBeams splitters) (0, fromList [start])
+part1 height start splitters = fst $ simulate height start splitters
 
-advanceBeams :: Set (Int, Int) -> (Int, Set (Int, Int)) -> (Int, Set (Int, Int))
-advanceBeams splitters (splitCount, beams) = (newSplitCount, newBeams)
+part2 :: Int -> (Int, Int) -> Set (Int, Int) -> Int
+part2 height start splitters = sum $ Data.Map.elems $ snd $ simulate height start splitters
+
+simulate height start splitters = last $ Prelude.take height $ iterate (advanceBeams splitters) initialState
   where
-    newSplitCount = splitCount + length (Data.Set.filter (\s -> length s == 2) newSplits)
-    newBeams = foldl1 union newSplits
-    newSplits = Data.Set.map (advanceBeam splitters) beams
+    initialState = (0, Data.Map.fromList [(start, 1)])
 
-advanceBeam :: Set (Int, Int) -> (Int, Int) -> Set (Int, Int)
-advanceBeam splitters (i, j) =
+advanceBeams :: Set (Int, Int) -> (Int, Map (Int, Int) Int) -> (Int, Map (Int, Int) Int)
+advanceBeams splitters (splitCount, beams) = (newSplitCount, mergedBeams)
+  where
+    newSplitCount = splitCount + length newSplits
+    newSplits = Data.Map.filter (\s -> length s == 2) newBeams
+    mergedBeams = Data.Map.unionsWith (+) newBeams
+    newBeams = Data.Map.mapWithKey (advanceBeam splitters) beams
+
+advanceBeam :: Set (Int, Int) -> (Int, Int) -> Int -> Map (Int, Int) Int
+advanceBeam splitters (i, j) superimposed =
   if (i + 1, j) `elem` splitters
-    then fromList [(i + 1, j - 1), (i + 1, j + 1)]
-    else fromList [(i + 1, j)]
+    then Data.Map.fromList [((i + 1, j - 1), superimposed), ((i + 1, j + 1), superimposed)]
+    else Data.Map.fromList [((i + 1, j), superimposed)]
